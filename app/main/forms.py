e95b79a73335b7deb1621fb8186b7544c8efe591
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-09-28 18:35:14 +0300

    deployment

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,7 +5,7 @@ class BlogForm(FlaskForm):
 
     title = StringField('BLOGmaster title',validators=[Required()])
     text = TextAreaField('write your Blog',validators=[Required()])
-    category= SelectField('Blog Category', choices=[('Select a category', 'Select a category'),('music', 'music'),('celebrites', 'celebrities'),('movie', 'movie'),('fashion', 'fashion'),('travel', 'travel'),('food', 'food'),('personal', 'personal'),('fitness', 'fitness'),('sports', 'sports')])
+    category= SelectField('Blog Category', choices=[('Select a category', 'Select a category'),('music', 'music'),('celebrity', 'celebrity'),('movie', 'movie'),('fashion', 'fashion'),('travel', 'travel'),('food', 'food'),('personal', 'personal'),('fitness', 'fitness'),('sports', 'sports')])
     submit = SubmitField('Submit')
 
 
